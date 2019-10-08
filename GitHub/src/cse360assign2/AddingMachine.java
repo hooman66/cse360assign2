07c7977d5ba9f576fac20278005cf254011f4a28
Hooman Abbasi <hooman.abbasi@me.com>
2019-10-07 18:52:30 -0700

    Second version
    1- The add method adds the parameter to the total variable
    2- The subtract method subtracts the parameter from the total variable
    3- The getTotal method returns the current total
    4- A history of the transactions will be kept to be returned by the
    toString method.

--- GitHub/src/cse360assign2/AddingMachine.java
+++ GitHub/src/cse360assign2/AddingMachine.java
@@ -1,27 +1,62 @@
+/**
+ * @author Hooman Abbasi
+ * CSE360-70641
+ * Assignment 2
+ * This program adds/subtracts given values and returns the total 
+ */
+
+
 package cse360assign2;
 
+/**
+ * This class adds/subtracts given values
+ * @param history keeps track of tranaction's history 
+ * @param total  is the total value after all transactions
+ */
 public class AddingMachine {
 	
 	private int total;
+	private String history = "0";
+	
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
-	
+	/**
+	 * 
+	 * @return total value 
+	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	/**
+	 * 
+	 * @param value will be added to the total value
+	 */
 	public void add (int value) {
 		
+		total = value + total;
+		history = history + " + " + value;
+		
 	}
 	
+	
+	/**
+	 * 
+	 * @param value will be subtracted from total value
+	 */
 	public void subtract (int value) {
 		
+		total = total - value;
+		history = history + " - " + value;
 	}
 		
+	/**
+	 * @param retrun's history of transactions
+	 */
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	public void clear() {
